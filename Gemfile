a68501f8181731ed5b5acf454f46733ac21f940e
Jennifer Vendetti <vendetti@stanford.edu>
2016-12-06 13:17:32 -0800

    Result of rails app:update. NCBO-1830.

--- Gemfile
+++ Gemfile
@@ -2,13 +2,16 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '5.0.0.1'
-
+# Use mysql as the database for Active Record
+gem 'mysql2', '>= 0.3.13', '< 0.5'
+# Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
+# Use Uglifier as compressor for JavaScript assets
+gem 'uglifier', '>= 1.3.0'
+# Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.1.0'
-gem 'uglifier', '>= 1.0.3'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
-# gem 'duktape'
 
 gem 'jquery-ui-rails'
 gem 'jquery-rails'
@@ -26,7 +29,6 @@ gem "pry"
 gem "iconv"
 gem "recaptcha", "= 0.4.0"
 gem "rest-client", "~> 1.8.0"
-gem "mysql", "~> 2.9.1"
 gem "i18n", "~> 0"
 gem "haml", "~> 4.0.0"
 gem "will_paginate", "~> 3.0"
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -6,5 +6,5 @@
 # no regular words or you'll be exposed to dictionary attacks.
 
 # TODO: Remove config.secret_token line after successful transition to Rails 4.
-BioportalWebUi::Application.config.secret_token = '4525b97b2408fe67fac6bee60bf80fd1e321db2671c6d1edcc3987e1a54bb17e3eeffe9fa402e9054fb76b0dbc0d71c503a2614dffddc4459e9308d677e34fd3'
-BioportalWebUi::Application.config.secret_key_base = 'xxx4525b97b2408fe67fac6bee60bf80fd1e321db2671c6d1edcc3987e1a54bb17e3eeffe9fa402e9054fb76b0dbc0d71c503a2614dffddc4459e9308d677e34fd3'
+# BioportalWebUi::Application.config.secret_token = '4525b97b2408fe67fac6bee60bf80fd1e321db2671c6d1edcc3987e1a54bb17e3eeffe9fa402e9054fb76b0dbc0d71c503a2614dffddc4459e9308d677e34fd3'
+# BioportalWebUi::Application.config.secret_key_base = 'xxx4525b97b2408fe67fac6bee60bf80fd1e321db2671c6d1edcc3987e1a54bb17e3eeffe9fa402e9054fb76b0dbc0d71c503a2614dffddc4459e9308d677e34fd3'
